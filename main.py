47df7276d3052d0c198ec31f977eb4e676661938
Ny Haingo Andry <haingo@aims.ac.za>
2025-06-19 12:40:39 +0200

    Add functionality to delete individual CSV files after processing

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.pdf_processor import extract_tables_from_pdf
 from src.data_cleaner import clean_and_combine_csvs, assign_event_types
 from src.utils.file_manip import delete_directory
+from src.utils.file_manip import delete_individual_csv
 from src.config import INPUT_PDF_PATH, OUTPUT_DIR, COMBINED_OUTPUT_FILE
 
 logging.basicConfig(level=logging.INFO)
@@ -14,11 +15,11 @@ _log = logging.getLogger(__name__)
 def main():
     start_time = time.time()
 
-    # Delete scratch directory if it exists
+    # Delete directory if it exists
     delete_directory(OUTPUT_DIR)
 
     # Extract tables from PDF
-    extract_tables_from_pdf(INPUT_PDF_PATH, OUTPUT_DIR)
+    csv_files = extract_tables_from_pdf(INPUT_PDF_PATH, OUTPUT_DIR)
 
     # Clean and combine CSV files
     clean_and_combine_csvs(OUTPUT_DIR, COMBINED_OUTPUT_FILE)
@@ -32,6 +33,9 @@ def main():
     # Save final combined data
     combined_df.to_csv(COMBINED_OUTPUT_FILE, index=False)
     _log.info(f"Final combined data saved to {COMBINED_OUTPUT_FILE}")
+
+    # delete individual CSV files
+    delete_individual_csv(csv_files, COMBINED_OUTPUT_FILE)
 
     end_time = time.time() - start_time
     _log.info(f"Total processing time: {end_time:.2f} seconds")
--- src/utils/file_manip.py
+++ src/utils/file_manip.py
@@ -6,3 +6,12 @@ def delete_directory(directory: Path):
     if directory.exists():
         shutil.rmtree(directory)
         print(f"Deleted directory: {directory}")
+
+
+def delete_individual_csv(individual_csv_files: list[Path], combined_file: Path):
+    if combined_file.exists() and combined_file.stat().st_size > 0:
+        for csv_file in individual_csv_files:
+            try:
+                csv_file.unlink()
+            except Exception as _:
+                pass
