f2aa3ed33afe2418a8d85d69167c53888d2cb980
Ny Haingo Andry <haingo@aims.ac.za>
2025-06-19 11:54:57 +0200

    return list of paths

--- src/pdf_processor.py
+++ src/pdf_processor.py
@@ -21,6 +21,7 @@ def convert_document_and_export_tables(input_doc_path: Path, output_dir: Path):
 
     doc_filename = conv_res.input.file.stem
 
+    csv_files: list[Path] = []
     for table_ix, table in enumerate(conv_res.document.tables):
         table_df: pd.DataFrame = table.export_to_dataframe()
         print(f"## Table {table_ix}")
@@ -29,6 +30,9 @@ def convert_document_and_export_tables(input_doc_path: Path, output_dir: Path):
         element_csv_filename = output_dir / f"{doc_filename}-table-{table_ix + 1}.csv"
         _log.info(f"Saving CSV table to {element_csv_filename}")
         table_df.to_csv(element_csv_filename, index=False)
+        csv_files.append(element_csv_filename)
 
     elapsed_time = time.time() - start_time
     _log.info(f"Document converted and tables exported in {elapsed_time:.2f} seconds.")
+
+    return csv_files
